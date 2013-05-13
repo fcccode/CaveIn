169fbe636f5c182f274f3cce3187ea2739759c55
Jake <jakemorey@hotmail.co.uk>
2013-05-13 01:51:30 +0100

    more comments

--- CaveIn/Source/Horror.cpp
+++ CaveIn/Source/Horror.cpp
@@ -6,35 +6,10 @@
 	Author:		Jake Morey
 	
 	Description:
-	
+	implements the class described in Horrow.hpp.
+	plays the audio after a random period of time.
+	influenced by Birds.cpp
 *********************************************************************/
-/*
-	file:	Bird.cpp
-	Version:	1.0
-	Date:	11th January 2013.
-	Author:	allan c. Milne.
-
-	Exposes:	Bird implementation.
-	Requires:	Sounds/bird.wav, XACore, XASound.
-
-	Description:
-	Implements the AudioRenderable Bird class that will play a bird chirp moving towards and away from the listener.
-	A XASound object is used to model the bird sound sample.
-	Uses volume adjustment to provide the perception of movement.
-	
-	The constructor sets up the XASound object.
-	Play() starts the sound playing.
-	AudioRender(...) implements the dynamic movement.
-
-	The volume (amplitude multiplier) is adjusted by applying a further multiplier:
-	*	0.8 (4/5) decreasing multiplier;
-	*	1.25 (5/4) increasing multiplier;
-	i.e. value will go 1.0, 0.8, 0.64, 0.512, 0.4096, .. and then up again.
-	Volume is adjusted every second.
-	Note these values have been used due to the effect they produce rather than being derived from the underlying physics of bird speed, etc.
-*/
-
-
 #include "XACore.hpp"
 #include "XASound.hpp"
 using AllanMilne::Audio::XACore;
@@ -42,7 +17,7 @@ using AllanMilne::Audio::XASound;
 
 #include "Horror.hpp"
 
-//--- Called every game frame to perform the audio animation of the bird's movement.
+//--- Called every game frame to check if the audio can be played again.
 //--- Parameter is time in seconds since last call.
 void Horror::RenderAudio (const float deltaTime)
 {
@@ -57,12 +32,9 @@ void Horror::RenderAudio (const float deltaTime)
 	}
 } // end RenderAudio function.
 
-//--- indicates if the object has been created correctly.
-inline bool Horror::IsOk () const { return (mHorror != NULL); }
-
-	//--- constructor will setup the bird sound object.
+//--- constructor sets up the XASound object.
 Horror::Horror (XACore *aCore)
-	: mHorror (NULL), mElapsedTime (0.0f), mVolumeAdjustment (1.1f), mPauseTime(1.0f)
+	: mHorror (NULL), mElapsedTime (0.0f), mPauseTime(1.0f)
 {
 	mHorror = aCore->CreateSound("Sounds/Atmosphere/Horror.wav");
 } // end constructor function.
@@ -76,7 +48,7 @@ Horror::~Horror()
 	}
 } // end destructor function.
 
-//--- Starts the bird sound, does not initiate any movement.
+//--- Starts the horror sound
 void Horror::Play (){
 	mHorror->Play(0);
 } // end Play method.
--- CaveIn/Source/Horror.hpp
+++ CaveIn/Source/Horror.hpp
@@ -6,26 +6,10 @@
 	Author:		Jake Morey
 	
 	Description:
-	
+	this class inherits from the AudioRenderable class.
+	plays the audio after a random period of time.
+	influenced by Birds.cpp
 *********************************************************************/
-/*
-	file:	Bird.hpp
-	Version:	1.1
-	Date:	6th March 2013.
-	Author:	allan c. Milne.
-
-	Exposes:	Bird.
-	Requires:	AudioRenderable, XACore, XASound.
-
-	Description:
-	Defines an AudioRenderable class that will play a bird chirp moving towards and away from the listener.
-	A XASound object is used to model the bird sound sample.
-	Uses volume adjustment to provide the perception of movement.
-
-	The constructor sets up the XASound object.
-	Play() starts the sound playing.
-	AudioRender(...) implements the dynamic movement.
-*/
 
 #ifndef __HORROR_HPP_1_1__
 #define __HORROR_HPP_1_1__
@@ -38,25 +22,24 @@ class XASound;
 class Horror : public AudioRenderable
 {
 public:
-	//--- Called every game frame to perform the audio animation of the bird's movement.
+	//--- Called every game frame to check if the audio can be played again.
 	//--- Parameter is time in seconds since last call.
 	void RenderAudio (const float deltaTime);
 
 	//--- indicates if the object has been created correctly.
-	bool IsOk () const;
+	inline bool IsOk () const { return (mHorror != NULL); }
 
 	//--- constructor sets up the XASound object.
 	Horror (XACore *aCore);
 	virtual ~Horror();
 
-	//--- Starts the bird sound, does not initiate any movement.
+	//--- Starts the horror sound
 	void Play();
 
 private:
-	XASound *mHorror;					// the bird sound.
-	float mElapsedTime;				// time since last bird movement applied.
-	float mVolumeAdjustment;		// amount of volume to apply for one movement, is changed to indicate "bouncing" of the sound away/towards the listener.
+	XASound *mHorror;				// the horror sound.
+	float mElapsedTime;				// time since last horror movement applied.	
 	float mPauseTime;
-};	// end Bird class.
+};	// end class.
 
 #endif
